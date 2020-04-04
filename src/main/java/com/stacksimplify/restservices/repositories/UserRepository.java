421ff72e0a0610b31b68636e218a737560712c5e
empopa <emilpopa24@yahoo.com>
2020-04-05 01:02:04 +0300

    JPA Base RestServices

---
+++ src/main/java/com/stacksimplify/restservices/repositories/UserRepository.java
@@ -0,0 +1,18 @@
+package com.stacksimplify.restservices.repositories;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.stacksimplify.restservices.entities.User;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+	
+	public User findByUsername(String username);
+	
+	 
+	
+
+}
---
+++ src/main/resources/data.sql
@@ -0,0 +1,3 @@
+insert into user values(101, 'emilpopa24@yahoo.com', 'Emil','Popa','admin','ssn101','epopa');
+insert into user values(102, 'mihaitrandafir24@yahoo.com', 'Mihai','Trandafir','admin','ssn102','mtrandafir');
+insert into user values(103, 'iustindiaconu24@yahoo.com', 'Iustin','Diaconu','admin','ssn103','idiaconu');
