4f14c656b64fab0fef1c136ad3e23b4fad054764
empopa <emilpopa24@yahoo.com>
2020-04-04 17:10:22 +0300

    Hello World Base

---
+++ src/main/java/com/stacksimplify/restservices/Hello/HelloWorldController.java
@@ -0,0 +1,18 @@
+package com.stacksimplify.restservices.Hello;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+
+	@GetMapping("/helloworld")
+	public String helloWorld() {
+		return "Hello World1";
+	}
+	
+	@GetMapping("/helloworld-bean")
+	public UserDetails helloWorldBean() {
+		return new UserDetails("Emil","Popa","Nucet");
+	}
+}
